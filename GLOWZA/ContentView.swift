ed2567cce7884929204b45ffafdf53185c25ed10
AsiniPerera <umayaperera423@gmail.com>
2026-04-17 09:34:36 +0530

    Initial Commit

---
+++ GLOWZA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GLOWZA
+//
+//  Created by COBSCCOMP242P-024 on 2026-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GLOWZA/GLOWZAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLOWZAApp.swift
+//  GLOWZA
+//
+//  Created by COBSCCOMP242P-024 on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct GLOWZAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
